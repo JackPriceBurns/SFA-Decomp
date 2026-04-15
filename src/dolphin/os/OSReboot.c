27b63d0f298c27df664ce0027dcc0084bccf1177
Zachary Canann <zacharycanann@gmail.com>
2026-04-14 18:37:14 -0700

    Retarget OSReboot to SFA SDK subset

--- src/dolphin/os/OSReboot.c
+++ src/dolphin/os/OSReboot.c
@@ -10,10 +10,9 @@ extern u8 g_unk_800030E2 AT_ADDRESS(0x800030E2);
 extern u32 g_unk_817FFFF8 AT_ADDRESS(0x817FFFF8);
 extern u32 g_unk_817FFFFC AT_ADDRESS(0x817FFFFC);
 
-int Prepared[2];
-
-void* SaveEnd;
-void* SaveStart;
+static void* SaveStart;
+static void* SaveEnd;
+static volatile BOOL Prepared;
 
 typedef struct {
     char date[16];
@@ -23,48 +22,35 @@ typedef struct {
     u32 reserved2;
 } AppLoaderStruct;
 
-AppLoaderStruct FatalParam ATTRIBUTE_ALIGN(32);
+static AppLoaderStruct FatalParam ATTRIBUTE_ALIGN(32);
 
-asm void Run(register void* entryPoint) {
-    nofralloc
-
-    sync
-    isync
-    mtlr entryPoint
-    blr
-}
-
-void Callback(s32, DVDCommandBlock*) {
-    Prepared[0] = TRUE;
-}
-
-static inline int IsStreamEnabled(void) {
-    if (DVDGetCurrentDiskID()->streaming) {
-        return TRUE;
+#pragma dont_inline on
+static asm void myFunc() { }
+static void Run(register void* entryPoint) {
+    OSDisableInterrupts();
+    ICFlashInvalidate();
+    asm {
+        sync
+        isync
+        mtlr entryPoint
+        blr
     }
+}
+#pragma dont_inline reset
 
-    return FALSE;
+static void Callback(s32 result, DVDCommandBlock* block) {
+    (void)result;
+    (void)block;
+    Prepared = TRUE;
 }
 
-/*
- * --INFO--
- * PAL Address: TODO
- * PAL Size: TODO
- * EN Address: TODO
- * EN Size: TODO
- * JP Address: TODO
- * JP Size: TODO
- */
 void __OSReboot(u32 resetCode, u32 bootDol) {
     OSContext exceptionContext;
-    DVDCommandBlock streamCancelBlock;
     DVDCommandBlock appLoaderReadBlock;
     DVDCommandBlock rebootReadBlock;
     u32 rebootSize;
+    u32 state;
     u32 offset;
-#if SDK_REVISION < 1
-    OSTime start;
-#endif
 
     (void)resetCode;
     (void)bootDol;
@@ -82,91 +68,52 @@ void __OSReboot(u32 resetCode, u32 bootDol) {
 
     DVDInit();
     DVDSetAutoInvalidation(TRUE);
-    DVDResume();
 
-    Prepared[0] = FALSE;
+    Prepared = FALSE;
     __DVDPrepareResetAsync(Callback);
+
+    if (!DVDCheckDisk()) {
+        __OSDoHotReset(g_unk_817FFFFC);
+    }
+
     __OSMaskInterrupts(0xFFFFFFE0);
     __OSUnmaskInterrupts(0x400);
     OSEnableInterrupts();
 
-#if SDK_REVISION < 1
-    start = OSGetTime();
-#endif
-
-    while (Prepared[0] != TRUE) {
-#if SDK_REVISION < 1
-        if (!DVDCheckDisk() || OS_TIMER_CLOCK < (OSGetTime() - start))
-#else
-        if (!DVDCheckDisk())
-#endif
-        {
-            __OSDoHotReset(g_unk_817FFFFC);
-        }
+    while (!Prepared) {
     }
 
-    if (!__OSIsGcam && IsStreamEnabled()) {
-        AISetStreamVolLeft(0);
-        AISetStreamVolRight(0);
-        DVDCancelStreamAsync(&streamCancelBlock, NULL);
-
-#if SDK_REVISION < 1
-        start = OSGetTime();
-#endif
-
-        while (DVDGetCommandBlockStatus(&streamCancelBlock)) {
-#if SDK_REVISION < 1
-            if (!DVDCheckDisk() || OS_TIMER_CLOCK < (OSGetTime() - start))
-#else
-            if (!DVDCheckDisk())
-#endif
-            {
-                __OSDoHotReset(g_unk_817FFFFC);
-            }
+    DVDReadAbsAsyncForBS(&appLoaderReadBlock, &FatalParam, sizeof(AppLoaderStruct), 0x2440, NULL);
+    while (TRUE) {
+        state = appLoaderReadBlock.state;
+        if (state == 0) {
+            break;
         }
-
-        AISetStreamPlayState(AI_STREAM_STOP);
-    }
-
-    DVDReadAbsAsyncPrio(&appLoaderReadBlock, &FatalParam, sizeof(AppLoaderStruct), 0x2440, NULL, 0);
-
-#if SDK_REVISION < 1
-    start = OSGetTime();
-#endif
-
-    while (DVDGetCommandBlockStatus(&appLoaderReadBlock)) {
-#if SDK_REVISION < 1
-        if (!DVDCheckDisk() || OS_TIMER_CLOCK < (OSGetTime() - start))
-#else
-        if (!DVDCheckDisk())
-#endif
-        {
-            __OSDoHotReset(g_unk_817FFFFC);
+        if (state == 1 || state >= 0xC) {
+            continue;
         }
+        __OSDoHotReset(g_unk_817FFFFC);
     }
 
     offset = FatalParam.size + 0x20;
     rebootSize = OSRoundUp32B(FatalParam.rebootSize);
-    DVDReadAbsAsyncPrio(&rebootReadBlock, (void*)0x81300000, rebootSize, offset + 0x2440, NULL, 0);
-
-#if SDK_REVISION < 1
-    start = OSGetTime();
-#endif
-
-    while (DVDGetCommandBlockStatus(&rebootReadBlock)) {
-#if SDK_REVISION < 1
-        if (!DVDCheckDisk() || OS_TIMER_CLOCK < (OSGetTime() - start))
-#else
-        if (!DVDCheckDisk())
-#endif
-        {
-            __OSDoHotReset(g_unk_817FFFFC);
+    DVDReadAbsAsyncForBS(&rebootReadBlock, (void*)0x81300000, rebootSize, offset + 0x2440, NULL);
+    while (TRUE) {
+        state = rebootReadBlock.state;
+        if (state == 0) {
+            break;
         }
+        if (state == 1 || state >= 0xC) {
+            continue;
+        }
+        __OSDoHotReset(g_unk_817FFFFC);
     }
 
     ICInvalidateRange((void*)0x81300000, rebootSize);
-
-    OSDisableInterrupts();
-    ICFlashInvalidate();
     Run((void*)0x81300000);
+}
+
+void OSSetSaveRegion(void* start, void* end) {
+    SaveStart = start;
+    SaveEnd = end;
 }
