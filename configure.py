24f6fc1817e15a2535aab0c077c14a3f8fd00f7a
Jack Price-Burns <jackpriceburns@outlook.com>
2026-04-21 02:17:27 +0100

    MSL string.c: switch to GC/1.3, invert strcmp final comparison — 100% match (7/7 fns)

--- configure.py
+++ configure.py
@@ -627,7 +627,7 @@ config.libs = [
             Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/mem_funcs.c", mw_version="GC/1.3"),
             Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/misc_io.c"),
             Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/printf.c"),
-            Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/string.c"),
+            Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/string.c", mw_version="GC/1.3"),
             Object(MatchingFor("GSAE01"), "dolphin/MSL_C/PPCEABI/bare/H/wchar_io.c"),
             Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/s_copysign.c"),
             Object(NonMatching, "dolphin/MSL_C/PPCEABI/bare/H/s_frexp.c"),
