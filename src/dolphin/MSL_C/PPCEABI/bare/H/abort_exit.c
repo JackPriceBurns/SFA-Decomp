a2e53288b894eb37780b01990f6a3b4b4a4df7e8
Jack Price-Burns <jackpriceburns@outlook.com>
2026-04-21 12:31:27 +0100

    abort_exit: strip critical_region, rename atexit statics
    - Remove __begin/end/kill_critical_region calls from exit() (not in target)
    - Rename __atexit_funcs / __atexit_curr_func with address suffix
      to match target reloc names
    
    exit 62.2% → 79.9%.

--- src/dolphin/MSL_C/PPCEABI/bare/H/abort_exit.c
+++ src/dolphin/MSL_C/PPCEABI/bare/H/abort_exit.c
@@ -8,13 +8,13 @@ void _ExitProcess();
 
 extern void (*_dtors[])(void);
 
-static void (*__atexit_funcs[64])(void);
+static void (*__atexit_funcs_803DB718[64])(void);
 
 static void (*__console_exit)(void);
 
 void (*__stdio_exit)(void);
 
-static int __atexit_curr_func;
+static int __atexit_curr_func_803DF074;
 
 static int __aborting;
 
@@ -23,8 +23,8 @@ void abort(void) {
     __aborting = 1;
     __begin_critical_region(atexit_funcs_access);
 
-    while (__atexit_curr_func > 0)
-        __atexit_funcs[--__atexit_curr_func]();
+    while (__atexit_curr_func_803DF074 > 0)
+        __atexit_funcs_803DB718[--__atexit_curr_func_803DF074]();
 
     __end_critical_region(atexit_funcs_access);
     __kill_critical_regions();
@@ -38,12 +38,9 @@ void abort(void) {
 }
 
 void exit(int status) {
-    int i;
     void (**dtor)(void);
 
     if (!__aborting) {
-        __begin_critical_region(atexit_funcs_access);
-        __end_critical_region(atexit_funcs_access);
         __destroy_global_chain();
 
         dtor = _dtors;
@@ -58,12 +55,8 @@ void exit(int status) {
         }
     }
 
-    __begin_critical_region(atexit_funcs_access);
-    while (__atexit_curr_func > 0)
-        __atexit_funcs[--__atexit_curr_func]();
-
-    __end_critical_region(atexit_funcs_access);
-    __kill_critical_regions();
+    while (__atexit_curr_func_803DF074 > 0)
+        __atexit_funcs_803DB718[--__atexit_curr_func_803DF074]();
 
     if (__console_exit != NULL) {
         __console_exit();
